e68fecbcf5d89eb00e6972df3cc86abebdaed00e
StanleeO <stanlee.okwii@gmail.com>
2021-08-27 13:26:24 +0300

    Update add contract modal to include gray footer

--- packages/ui/src/components/popcorn/emissions-dashboard/AddContractModal/index.tsx
+++ packages/ui/src/components/popcorn/emissions-dashboard/AddContractModal/index.tsx
@@ -7,7 +7,11 @@ export interface ContractModalProps {
   addContract: (contract: string) => void;
 }
 
-export const AddContractModal: React.FC<ContractModalProps> = ({ open, setOpen, addContract }) => {
+export const AddContractModal: React.FC<ContractModalProps> = ({
+  open,
+  setOpen,
+  addContract,
+}) => {
   const [contractAddress, setContractAddress] = useState<string>('');
 
   return (
@@ -28,9 +32,10 @@ export const AddContractModal: React.FC<ContractModalProps> = ({ open, setOpen, 
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-50 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
+          {/* This element is to trick the browser into centering the modal contents. */}
           <span
             className="hidden sm:inline-block sm:align-middle sm:h-screen"
             aria-hidden="true"
@@ -46,31 +51,33 @@ export const AddContractModal: React.FC<ContractModalProps> = ({ open, setOpen, 
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-              <div>
-                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <Dialog.Title
-                    as="h4"
-                    className="text-base text-center mb-8 leading-6 font-medium text-gray-800"
-                  >
-                    Add Contract to Dashboard
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-700 pb-1">
-                      Enter Ethereum Address of contract
-                    </p>
-                    <input
-                      type="text"
-                      onChange={event =>
-                        setContractAddress(event.target.value)
-                      }
-                      className="w-full pl-4 pr-10 py-2 text-sm leading-none border rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-500 font-light"
-                      placeholder="0xD634182479185918515"
-                    />
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title
+                      as="h4"
+                      className="text-base text-center mb-8 leading-6 font-medium text-gray-800"
+                    >
+                      Add Contract to Dashboard
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-700 pb-1">
+                        Enter Ethereum Address of contract
+                      </p>
+                      <input
+                        type="text"
+                        onChange={(event) =>
+                          setContractAddress(event.target.value)
+                        }
+                        className="w-full pl-4 pr-10 py-2 text-sm leading-none border rounded-lg shadow-sm focus:outline-none focus:shadow-outline text-gray-500 font-light"
+                        placeholder="0xD634182479185918515"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
                   className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
@@ -80,7 +87,7 @@ export const AddContractModal: React.FC<ContractModalProps> = ({ open, setOpen, 
                 </button>
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={() => setOpen(false)}
                 >
                   Cancel
